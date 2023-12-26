db2332e4f676f26db2eb620213dd7aac130f171d
unknown <mbah.noname@gmail.com>
2023-12-26 13:20:48 +0800

    add result layout

--- app/src/main/java/com/dicoding/atbquickscan/ui/ResultActivity.kt
+++ app/src/main/java/com/dicoding/atbquickscan/ui/ResultActivity.kt
@@ -2,11 +2,38 @@ package com.dicoding.atbquickscan.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.dicoding.atbquickscan.R
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var imageResult: ImageView
+    private lateinit var textResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        imageResult = findViewById(R.id.iv_result)
+        textResult = findViewById(R.id.tv_result)
+
+        val text = intent.getStringExtra("text")
+        val imageUrl = intent.getStringExtra("imageUrl")
+
+        textResult.text = text
+        Glide.with(this)
+            .load(imageUrl)
+            .into(imageResult)
+
+        supportActionBar?.apply {
+            title = "Result"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
